afc26bf94858b554f9b091746d14cb1d97c86809
shubham kharat <shubhamkharat5226@gmail.com>
2026-06-11 09:03:06 +0530

    Fix cloudinary version

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
