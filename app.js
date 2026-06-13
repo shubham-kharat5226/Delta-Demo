f979a6ed0844546e1b4013cef2ad5c0016be769c
shubham kharat <shubhamkharat5226@gmail.com>
2026-06-13 10:06:36 +0530

    updated website

--- app.js
+++ app.js
@@ -85,7 +85,6 @@ app.all("*", (req, res, next) => {
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Somthing went wrong!" } = err;
   res.status(statusCode).render("error.ejs", { message });
-  // res.status(statusCode).send(message);
 });
 
 const PORT = process.env.PORT || 8080;
--- cloudConfig.js
+++ cloudConfig.js
@@ -1,3 +1,25 @@
+// const cloudinary = require("cloudinary").v2;
+// const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUD_API_KEY,
+//   api_secret: process.env.CLOUD_API_SECRET,
+// });
+
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'wanderlust_DEV',
+//     allowedFormats: ['jpeg', 'png', 'jpg'],
+//   },
+// });
+
+// module.exports = {
+//   cloudinary,
+//   storage
+// };
+
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
@@ -7,16 +29,15 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
-    folder: 'wanderlust_DEV',
-    allowerdFormats: ['jpeg', 'png', 'jpg'],
+    folder: "wanderlust_DEV",
+    allowedFormats: ["jpeg", "png", "jpg"],
   },
 });
 
 module.exports = {
   cloudinary,
-  storage
+  storage,
 };
