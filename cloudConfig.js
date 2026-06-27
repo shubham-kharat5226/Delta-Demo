3299cc5f39cbbb638374b80816f085cac2aadb23
shubham kharat <shubhamkharat5226@gmail.com>
2026-06-28 01:09:43 +0530

    update project

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,11 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-  cloud_name:
-    process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY || process.env.CLOUD_API_KEY,
-  api_secret:
-    process.env.CLOUDINARY_SECRET || process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
